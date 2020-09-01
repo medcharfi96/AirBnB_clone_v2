54d7bc50d57d97f046d65126f14cb72dc0099aad
medcharfi96 <1389@holbertonschool.com>
2020-09-02 00:15:32 +0200

    add

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,8 +44,8 @@ def Number_root(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def HTML_root(n):
     """
-	return:  template if n is true
-	"""
+    return:  template if n is true
+    """
     if n is int:
             return render_template("5-number.html", n=n)
 
