d250e7b5e3a3e4f46418d6fbe5586ba0bdff96df
medcharfi96 <1389@holbertonschool.com>
2020-09-01 23:28:06 +0200

    add

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+task 2
+"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """
+    return : hello HBNb
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def only_HBNB():
+    """
+    return : hello HBNb
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_replace_text():
+    """
+    return : c is text
+    """
+    text_to_replace = text.replace("_", " ")
+    return "C {}".format(text_to_replace)
+
+if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000)
